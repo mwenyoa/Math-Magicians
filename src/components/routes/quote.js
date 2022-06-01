58b3c938b5af2eeb09c2f169da9b975a334e8b81
Anthony Mwenyo <mwenyoanthony@gmail.com>
2022-06-01 14:44:05 +0200

    Add quote  functional component and content

---
+++ src/components/routes/quote.js
@@ -0,0 +1,12 @@
+import React from 'react';
+
+const Quote = () => (
+  <div className="qoute-section">
+    <p>
+      Mathematic is not about numbers, equations, or
+      algorithms. It is about understanding. -William Paul Thustom.
+    </p>
+  </div>
+);
+
+export default Quote;
